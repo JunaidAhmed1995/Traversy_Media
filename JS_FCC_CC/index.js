921a1061034a68c396537e5fc312040007644cfd
Junaid Ahmed <junaid@donesol.co.uk>
2021-08-02 10:57:33 +0500

    3 ways to declare a variable [var, let, const]

--- JS_FCC_CC/index.js
+++ JS_FCC_CC/index.js
@@ -9,3 +9,13 @@
  ?6: symbol, 
  ?7: object
  */
+
+//*Variable's is JS
+//There are three ways to declare variables in Java-Script
+//?1: var
+//?2: let
+//?3: const
+//var has global scope. let and const have block scope.
+//var variables can re-assign and re-declare
+//let variables can re-assign BUT cannot re-declare in the same scope
+//const variables cannot re-assign or re-declare in the same scope
