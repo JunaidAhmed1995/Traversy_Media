fb42545424d2b137f3dee2fbda225b9f9972657b
Junaid Ahmed <junaid@donesol.co.uk>
2021-08-02 10:56:30 +0500

    comments and primitive data types in js

---
+++ JS_FCC_CC/index.js
@@ -0,0 +1,11 @@
+//Single Line COmment. Below is multi-line comment
+/*
+ * Primitive Data Types in JS
+ ?1: undefined, 
+ ?2: null, 
+ ?3: boolean, 
+ ?4: string, 
+ ?5: number, 
+ ?6: symbol, 
+ ?7: object
+ */
